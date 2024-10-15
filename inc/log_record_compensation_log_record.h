4574bae3756be9a79be66c02fe33f95f5c83842a
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-15 10:56:58 +0530

    --

--- inc/log_record_compensation_log_record.h
+++ inc/log_record_compensation_log_record.h
@@ -6,7 +6,7 @@ struct compensation_log_record
 {
 	uint256 prev_log_record; // LSN of the previous log record in the WALe for this very same mini transactionss
 
-	uint256 compensation_of; // this log record is undo log record of
+	uint256 undo_of; // this log record is undo log record of
 
 	uint256 next_log_record_to_undo; // this is the prev_log_record value of the log record at compensation_of
 };
