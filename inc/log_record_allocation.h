895d0ce09d7d7e4b923bb77b33737374b9dbe988
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-15 09:24:29 +0530

    minor comment change

--- inc/log_record_allocation.h
+++ inc/log_record_allocation.h
@@ -1,13 +1,12 @@
 #ifndef WALE_ALLOCATION_LOG_RECORD_H
 #define WALE_ALLOCATION_LOG_RECORD_H
 
+// log record struct for PAGE_ALLOCATION and PAGE_DEALLOCATION
 typedef struct allocation_log_record allocation_log_record;
 struct allocation_log_record
 {
 	uint256 prev_log_record; // LSN of the previous log record in the WALe for this very same mini transaction
 	uint64_t page_id;
 };
-
-// this log record is used for both allocation and deallocation of a page
 
 #endif
