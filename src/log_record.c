48c0499027f53685e4f2b659bf4fd9674fea8a0e
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-15 18:11:24 +0530

    started with implementing getter for tuple defs for log record

--- src/log_record.c
+++ src/log_record.c
@@ -1,3 +1,15 @@
 #include<log_record.h>
 
-log_record_tuple_defs initialize_log_record_tuple_defs(const mini_transaction_engine_stats* stats);
+log_record_tuple_defs initialize_log_record_tuple_defs(const mini_transaction_engine_stats* stats)
+{
+	log_record_tuple_defs lrtd = {};
+
+	// first initialize the dtis required
+	lrtd.page_id_type = define_uint_non_nullable_type("page_id", stats->page_id_width);
+	lrtd.LSN_type = define_large_uint_non_nullable_type("LSN", stats->log_sequence_number_width);
+	lrtd.data_in_bytes_type = get_variable_length_blob_type("data", stats->page_size);
+	lrtd.size_info_in_bytes_type = get_variable_length_blob_type("size_info", 13);
+	lrtd.type_info_in_bytes_type = get_variable_length_blob_type("type_info", stats->page_size);
+
+	return lrtd;
+}
