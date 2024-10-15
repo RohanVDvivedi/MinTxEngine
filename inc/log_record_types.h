1803e5235e8a8fe4ddf1934f1fda7ac90a439f1a
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-15 09:27:33 +0530

    added numbering to the record type enum

--- inc/log_record_types.h
+++ inc/log_record_types.h
@@ -4,24 +4,27 @@
 typedef enum wale_log_record_type wale_log_record_type;
 enum wale_log_record_type
 {
-	PAGE_ALLOCATION,
-	PAGE_DEALLOCATION,
+	PAGE_ALLOCATION = 1,
+	PAGE_DEALLOCATION = 2,
 
-	PAGE_INIT,
+	PAGE_INIT = 3,
 
-	TUPLE_APPEND,
-	TUPLE_INSERT,
-	TUPLE_UPDATE,
-	TUPLE_DISCARD,
+	TUPLE_APPEND = 4,
+	TUPLE_INSERT = 5,
+	TUPLE_UPDATE = 6,
+	TUPLE_DISCARD = 7,
 
-	TUPLE_DISCARD_ALL,
-	TUPLE_DISCARD_TRAILING_TOMB_STONES,
+	TUPLE_DISCARD_ALL = 8,
+	TUPLE_DISCARD_TRAILING_TOMB_STONES = 9,
 
-	TUPLE_SWAP,
+	TUPLE_SWAP = 10,
 
-	TUPLE_UPDATE_ELEMENT_IN_PLACE,
+	TUPLE_UPDATE_ELEMENT_IN_PLACE = 11,
 
-	PAGE_CLONE,
+	PAGE_CLONE = 12,
+
+	FULL_PAGE_WRITE = 13,
+	// this log record is first written for any page type, the first time it becomes dirty after a checkpoint
 };
 
 #endif
