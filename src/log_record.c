75d71510528a25ed7c482e2476062a0221566724
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-16 11:44:21 +0530

    implemented serialization for full page write

--- src/log_record.c
+++ src/log_record.c
@@ -1144,6 +1144,36 @@ const void* serialized_log_record(const log_record_tuple_defs* lrtd_p, const min
 			(*result_size) = get_tuple_size(&(lrtd_p->pclr_def), result + 1) + 1;
 			return result;
 		}
+		case FULL_PAGE_WRITE :
+		{
+			uint32_t capacity = 1 + get_minimum_tuple_size(&(lrtd_p->fpwlr_def));
+
+			void* result = malloc(capacity);
+			if(result == NULL)
+				goto ERROR;
+
+			((unsigned char*)result)[0] = FULL_PAGE_WRITE;
+
+			if(!set_element_in_tuple(&(lrtd_p->fpwlr_def), STATIC_POSITION(0), result + 1, &(user_value){.large_uint_value = lr->fpwlr.mini_transaction_id}, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->fpwlr_def), STATIC_POSITION(1), result + 1, &(user_value){.large_uint_value = lr->fpwlr.prev_log_record}, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->fpwlr_def), STATIC_POSITION(2), result + 1, &(user_value){.uint_value = lr->fpwlr.page_id}, UINT32_MAX))
+				goto ERROR;
+
+			user_value size_def = {.blob_value = (uint8_t [13]){}};
+			size_def.blob_size = serialize_tuple_size_def(&(lr->fpwlr.size_def), (void*)(size_def.blob_value));
+			if(!set_element_in_tuple(&(lrtd_p->fpwlr_def), STATIC_POSITION(3), result + 1, &size_def, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->fpwlr_def), STATIC_POSITION(4), result + 1, &(user_value){.blob_value = lr->fpwlr.page_contents, .blob_size = get_page_content_size_for_page(lr->fpwlr.page_id, stats)}, UINT32_MAX))
+				goto ERROR;
+
+			(*result_size) = get_tuple_size(&(lrtd_p->fpwlr_def), result + 1) + 1;
+			return result;
+		}
 	}
 
 	ERROR :;
