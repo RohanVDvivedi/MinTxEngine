0918fe2051bb4736b3e75c7d536bc7158dbf3106
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-16 11:48:17 +0530

    implemented CLR log serialization

--- src/log_record.c
+++ src/log_record.c
@@ -1174,6 +1174,31 @@ const void* serialized_log_record(const log_record_tuple_defs* lrtd_p, const min
 			(*result_size) = get_tuple_size(&(lrtd_p->fpwlr_def), result + 1) + 1;
 			return result;
 		}
+		case COMPENSATION_LOG :
+		{
+			uint32_t capacity = 1 + get_minimum_tuple_size(&(lrtd_p->clr_def));
+
+			void* result = malloc(capacity);
+			if(result == NULL)
+				goto ERROR;
+
+			((unsigned char*)result)[0] = COMPENSATION_LOG;
+
+			if(!set_element_in_tuple(&(lrtd_p->clr_def), STATIC_POSITION(0), result + 1, &(user_value){.large_uint_value = lr->clr.mini_transaction_id}, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->clr_def), STATIC_POSITION(1), result + 1, &(user_value){.large_uint_value = lr->clr.prev_log_record}, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->clr_def), STATIC_POSITION(2), result + 1, &(user_value){.large_uint_value = lr->clr.undo_of}, UINT32_MAX))
+				goto ERROR;
+
+			if(!set_element_in_tuple(&(lrtd_p->clr_def), STATIC_POSITION(3), result + 1, &(user_value){.large_uint_value = lr->clr.next_log_record_to_undo}, UINT32_MAX))
+				goto ERROR;
+
+			(*result_size) = get_tuple_size(&(lrtd_p->clr_def), result + 1) + 1;
+			return result;
+		}
 	}
 
 	ERROR :;
