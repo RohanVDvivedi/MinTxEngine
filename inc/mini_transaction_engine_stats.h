b4c959072de9e4898ef646371d20368efc330601
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-18 09:58:37 +0530

    mini transaction engine stats re structured

--- inc/mini_transaction_engine_stats.h
+++ inc/mini_transaction_engine_stats.h
@@ -4,11 +4,9 @@
 typedef struct mini_transaction_engine_stats mini_transaction_engine_stats;
 struct mini_transaction_engine_stats
 {
-	uint32_t log_sequence_number_width; // required to store log_sequence_number
-
-	uint32_t page_id_width; // bytes required to store page_id
-
 	uint32_t page_size; // size of page in bytes
+	uint32_t page_id_width; // bytes required to store page_id
+	uint32_t log_sequence_number_width; // required to store log_sequence_number
 };
 
 #endif
