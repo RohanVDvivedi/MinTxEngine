3a2950e17dfa30336fc14b58e47d5dce42827315
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-18 18:05:09 +0530

    segregated first block_id calculation into a separate static function

--- src/callbacks_bufferpool.c
+++ src/callbacks_bufferpool.c
@@ -2,10 +2,15 @@
 
 #include<block_io.h>
 
+static off_t get_first_block_id_for_page_id(uint64_t page_id, uint32_t page_size, size_t block_size)
+{
+	return ((page_id * page_size) / block_size) + 1; // this +1 ensures that we do not read/write the first read-only header block
+}
+
 int read_page_for_bufferpool(const void* page_io_ops_handle, void* frame_dest, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
-	off_t block_id = ((page_id * page_size) / block_size) + 1; // this +1 ensures that we do not write the first read-only header block
+	off_t block_id = get_first_block_id_for_page_id(page_id, page_size, block_size);
 	size_t block_count = page_size / block_size;
 	return read_blocks_from_block_file(((block_file*)(page_io_ops_handle)), frame_dest, block_id, block_count);
 }
@@ -13,7 +18,7 @@ int read_page_for_bufferpool(const void* page_io_ops_handle, void* frame_dest, u
 int write_page_for_bufferpool(const void* page_io_ops_handle, const void* frame_src, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
-	off_t block_id = ((page_id * page_size) / block_size) + 1;// this +1 ensures that we do not read the first read-only header block
+	off_t block_id = get_first_block_id_for_page_id(page_id, page_size, block_size);
 	size_t block_count = page_size / block_size;
 	return write_blocks_to_block_file(((block_file*)(page_io_ops_handle)), frame_src, block_id, block_count);
 }
