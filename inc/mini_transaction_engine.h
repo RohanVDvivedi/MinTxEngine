76ff9814a5feed986427b7b8b8216da7864d7d97
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-18 20:47:01 +0530

    bufferpool callbacks implemented

--- inc/mini_transaction_engine.h
+++ inc/mini_transaction_engine.h
@@ -7,6 +7,9 @@
 
 #include<rwlock.h>
 
+#include<wale.h>
+#include<bufferpool.h>
+
 #include<mini_transaction_engine_stats.h>
 #include<log_record.h>
 
@@ -49,6 +52,7 @@ struct mini_transaction_engine
 	// flushedLSN = max(flushedLSN, flush_all_log_records(wa_list.last().wale_handle));
 	// this is because we do not have a globally consistent view of the flushedLSN because global lock ges released while retrieveing it, and call to flushing log records will not ensure the return in any correct order, as global lock is released also while flushing the log records
 	// we can also not rely on the flushed LSN of the most recent wale_handle because if there were no log records writeen then it might even be 0, i.e. invalid
+	// if you are starting a new database it must be set to INVALID_LOG_SEQUENCE_NUMBER i.e. 0
 	uint256 flushedLSN;
 
 	// tuple definitions for the log records handled by this engine
@@ -56,14 +60,14 @@ struct mini_transaction_engine
 
 	// below three are the parts of mini_transaction table
 	hashmap writer_mini_transactions; // mini_transaction_id != 0, state = IN_PROGRESS or UNDOING_FOR_ABORT else if state = ABORTED or COMMITTED then waiters_count > 0
-	linked_list reader_mini_transactions; // mini_transaction_id == 0, state = IN_PROGRESS
+	linkedlist reader_mini_transactions; // mini_transaction_id == 0, state = IN_PROGRESS
 
-	linked_list free_mini_transactions_list; // list of free mini transactions, new mini transactions are assigned from this list, here waiters_count must be 0
+	linkedlist free_mini_transactions_list; // list of free mini transactions, new mini transactions are assigned from this list, here waiters_count must be 0
 
 	// below two are the parts of dirty page table
 	hashmap dirty_page_table;
 
-	linked_list free_dirty_page_entries_list; // list of free dirty page entries, new dirty page entrues are assigned from this lists or are allocated
+	linkedlist free_dirty_page_entries_list; // list of free dirty page entries, new dirty page entrues are assigned from this lists or are allocated
 
 	// new threads attempting to start a new mini transaction execution wait here until a slot is available
 	// a signal will be called everytime an insert is performed on free_mini_transactions_list
--- src/callbacks_bufferpool.c
+++ src/callbacks_bufferpool.c
@@ -28,3 +28,42 @@ int flush_all_pages_for_bufferpool(const void* page_io_ops_handle)
 {
 	return flush_all_writes_to_block_file(((block_file*)(page_io_ops_handle)));
 }
+
+#include<system_page_header_util.h>
+#include<mini_transaction_engine.h>
+
+int can_be_flushed_to_disk_for_bufferpool(void* flush_callback_handle, uint64_t page_id, const void* frame)
+{
+	mini_transaction_engine* mte = flush_callback_handle;
+
+	uint256 pageLSN = get_pageLSN_for_page(frame, &(mte->stats));
+
+	// if the pageLSN on the page is invalid, then there is a possibility that the page is not being tracked by ARIES algorithm (because it just came into use, and was dirtied for some reason) so it can be flushed to disk
+	if(are_equal_uint256(pageLSN, INVALID_LOG_SEQUENCE_NUMBER))
+		return 1;
+
+	// if the flushedLSN is 0, that means no logs were flushed yet, so you can not flush this page to disk
+	if(are_equal_uint256(mte->flushedLSN, INVALID_LOG_SEQUENCE_NUMBER))
+		return 0;
+
+	return compare_uint256(mte->flushedLSN, pageLSN) >= 0; // ARIES suggests that flushedLSN should be greater than or equal to pageLSN to allow the page to reach to the disk
+}
+
+void was_flushed_to_disk_for_bufferpool(void* flush_callback_handle, uint64_t page_id, const void* frame)
+{
+	mini_transaction_engine* mte = flush_callback_handle;
+
+	// the page that was flushed to disk, may not be dirty, so first find the corresponding entry from the dirty_page_table that matches the page_id
+	dirty_page_table_entry* entry = (dirty_page_table_entry*) find_equals_in_hashmap(&(mte->dirty_page_table), &(dirty_page_table_entry){.page_id = page_id});
+
+	// if no such entry is found in out dirty page table quit
+	if(entry == NULL)
+		return;
+
+	// else remove it from the dirty_page_table and insert it to the free_dirty_page_entries_list
+	// be mindful and reset the attributes of the entry
+	remove_from_hashmap(&(mte->dirty_page_table), entry);
+	entry->page_id = mte->user_stats.NULL_PAGE_ID;
+	entry->recLSN = INVALID_LOG_SEQUENCE_NUMBER;
+	insert_tail_in_linkedlist(&(mte->free_dirty_page_entries_list), entry);
+}
