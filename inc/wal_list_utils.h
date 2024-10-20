4c8c2ae5ef77bcf9bcb40f5479685189cc8cffef
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-20 18:48:33 +0530

    minor comments added

--- inc/wal_list_utils.h
+++ inc/wal_list_utils.h
@@ -8,11 +8,13 @@
 
 // create a new wal directory at (mte->database_file_name + "_logs/") and a new first log.7 (almost empty) wal file
 // it will fail if this directory already exists
+// it will also initialize flushedLSN and checkpointLSN attributes of mte, on success
 int create_new_wal_list(mini_transaction_engine* mte);
 
 // fails if the database_file_name + "/logs/" directory does not exists OR is empty
 // it will also fail if any of the wal file is not named according to their first LSNs
 // of if their log_sequence_number_widths do not match with the one in the stats of mte
+// it will also initialize flushedLSN and checkpointLSN attributes of mte, on success
 int initialize_wal_list(mini_transaction_engine* mte);
 
 // returns index of the wal_accessor from the wal_list that may include LSN
