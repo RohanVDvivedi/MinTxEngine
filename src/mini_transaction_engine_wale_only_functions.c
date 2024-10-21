abf70264a98d4463065c7b0d9b28671d5b4f256b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-21 18:52:03 +0530

    started implementing wale only mte functions

--- src/mini_transaction_engine_wale_only_functions.c
+++ src/mini_transaction_engine_wale_only_functions.c
@@ -46,3 +46,63 @@ static uint256 log_the_already_applied_log_record_for_mini_transaction_and_manag
 
 	return log_record_LSN;
 }
+
+int init_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, uint32_t page_header_size, const tuple_size_def* tpl_sz_d)
+{
+	// TODO
+}
+
+void set_page_header_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const void* hdr, int* abort_error)
+{
+	// TODO
+}
+
+int append_tuple_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, const void* external_tuple)
+{
+	// TODO
+}
+
+int insert_tuple_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple)
+{
+	// TODO
+}
+
+int update_tuple_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple)
+{
+	// TODO
+}
+
+int discard_tuple_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, uint32_t index)
+{
+	// TODO
+}
+
+void discard_all_tuples_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d)
+{
+	// TODO
+}
+
+uint32_t discard_trailing_tomb_stones_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d)
+{
+	// TODO
+}
+
+int swap_tuples_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, uint32_t i1, uint32_t i2)
+{
+	// TODO
+}
+
+int set_element_in_tuple_in_place_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_def* tpl_d, uint32_t tuple_index, positional_accessor element_index, const user_value* value)
+{
+	// TODO
+}
+
+void clone_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, const tuple_size_def* tpl_sz_d, const void* page_contents_src)
+{
+	// TODO
+}
+
+int run_page_compaction_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, uint32_t page_size, const tuple_size_def* tpl_sz_d)
+{
+	// TODO
+}
