cdfa8489a4bb13eb40961688ab3ee3e25fdc3e55
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-22 11:51:41 +0530

    minor comment added

--- inc/mini_transaction_engine_util.h
+++ inc/mini_transaction_engine_util.h
@@ -5,7 +5,7 @@
 
 // it is an unsafe function, must be held with global lock held
 // it marks the page as dirty in the dirty page table and bufferpool both as dirty
-// it is expected that page contains the pageLSN that made it dirty
+// it is expected that page contains the pageLSN that made it dirty, so the newly inserted dirty page table entry (if any) has its recLSN as pageLSN of the page
 // if the page is already in dirty_page_table, then its recLSN is not updated
 // it is expected that the page is already write latched
 void mark_page_as_dirty_in_bufferpool_and_dirty_page_table_UNSAFE(mini_transaction_engine* mte, void* page, uint64_t page_id);
