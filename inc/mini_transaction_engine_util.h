60b560c6bb6175546f618164971cbde5b6a76bc0
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-23 18:44:25 +0530

    --

--- inc/mini_transaction_engine_util.h
+++ inc/mini_transaction_engine_util.h
@@ -15,7 +15,7 @@ void mark_page_as_dirty_in_bufferpool_and_dirty_page_table_UNSAFE(mini_transacti
 // this function reads the writerLSN on the page and tries to figure out if there is an active transactions that has this page persistently write locked
 // is so a pointer to this transaction is returned
 // this function call does not affect the reference_counter of that mini transaction, hence the return values must not be used post release of the global lock
-// a standard correct procedure would be to get the locker mini transaction, then relese latch on the page and then wait for the this mini transaction to complete (using the function below)
+// a standard correct procedure would be to get the locker mini transaction, then relese latch on the page and then wait for the this mini transaction to complete (using the function wait_for_mini_transaction_completion_UNSAFE() below)
 mini_transaction* get_mini_transaction_that_last_persistent_write_locked_this_page_UNSAFE(mini_transaction_engine* mte, void* page);
 
 // decrement the reference_counter of the mini transaction
