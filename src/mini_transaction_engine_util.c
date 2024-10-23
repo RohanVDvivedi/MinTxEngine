18b512fd7f8ee1ed5cdb9f8353f2b39d399b0aa8
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-23 19:34:29 +0530

    segregated logic into 2 parts

--- src/mini_transaction_engine_util.c
+++ src/mini_transaction_engine_util.c
@@ -73,27 +73,33 @@ int wait_for_mini_transaction_completion_UNSAFE(mini_transaction_engine* mte, mi
 		struct timespec current_time;
 		clock_gettime(CLOCK_REALTIME, &current_time);
 
-		unsigned long long int secs = write_lock_wait_timeout_in_microseconds_LEFT / 1000000;
-		unsigned long long int nano_secs_extra = (write_lock_wait_timeout_in_microseconds_LEFT % 1000000) * 1000;
+		// make an attempt for atleast write_lock_wait_timeout_in_microseconds_LEFT
+		{
+			unsigned long long int secs = write_lock_wait_timeout_in_microseconds_LEFT / 1000000;
+			unsigned long long int nano_secs_extra = (write_lock_wait_timeout_in_microseconds_LEFT % 1000000) * 1000;
 
-		struct timespec wait_till = {.tv_sec = (current_time.tv_sec + secs), .tv_nsec = (current_time.tv_nsec + nano_secs_extra)};
+			struct timespec wait_till = {.tv_sec = (current_time.tv_sec + secs), .tv_nsec = (current_time.tv_nsec + nano_secs_extra)};
 
-		// do timedwait on job_queue_empty_wait, while releasing job_queue_mutex, while we wait
-		wait_error = pthread_cond_timedwait(&(mt->write_lock_wait), &(mte->global_lock), &wait_till);
+			// do timedwait
+			wait_error = pthread_cond_timedwait(&(mt->write_lock_wait), &(mte->global_lock), &wait_till);
+		}
 
-		// calculate current time after wait
-		struct timespec current_time_after_wait;
-		clock_gettime(CLOCK_REALTIME, &current_time_after_wait);
+		// substract elapsed time from write_lock_wait_timeout_in_microseconds_LEFT
+		{
+			// calculate current time after wait
+			struct timespec current_time_after_wait;
+			clock_gettime(CLOCK_REALTIME, &current_time_after_wait);
 
-		// now calculate the time elapsed
-		uint64_t microseconds_elapsed = (((int64_t)current_time_after_wait.tv_sec - (int64_t)current_time.tv_sec) * INT64_C(1000000))
-		+ (((int64_t)current_time_after_wait.tv_nsec - (int64_t)current_time_after_wait.tv_nsec) / INT64_C(1000));
+			// now calculate the time elapsed
+			uint64_t microseconds_elapsed = (((int64_t)current_time_after_wait.tv_sec - (int64_t)current_time.tv_sec) * INT64_C(1000000))
+			+ (((int64_t)current_time_after_wait.tv_nsec - (int64_t)current_time_after_wait.tv_nsec) / INT64_C(1000));
 
-		// discard the time used
-		if(microseconds_elapsed > write_lock_wait_timeout_in_microseconds_LEFT)
-			write_lock_wait_timeout_in_microseconds_LEFT = 0;
-		else
-			write_lock_wait_timeout_in_microseconds_LEFT -= microseconds_elapsed;
+			// discard the time used
+			if(microseconds_elapsed > write_lock_wait_timeout_in_microseconds_LEFT)
+				write_lock_wait_timeout_in_microseconds_LEFT = 0;
+			else
+				write_lock_wait_timeout_in_microseconds_LEFT -= microseconds_elapsed;
+		}
 	}
 
 	// collect the result as, decrement_mini_transaction_reference_counter_UNSAFE() may destroy it
