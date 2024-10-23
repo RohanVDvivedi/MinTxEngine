72bbed1728d79fa7f1e681945dfde30c4278bb62
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-23 18:43:36 +0530

    added a utility function to decrement reference counter for a mini transaction

--- src/mini_transaction_engine_util.c
+++ src/mini_transaction_engine_util.c
@@ -39,6 +39,24 @@ mini_transaction* get_mini_transaction_that_last_persistent_write_locked_this_pa
 	return mt;
 }
 
-void decrement_mini_transaction_reference_counter_UNSAFE(mini_transaction_engine* mte, mini_transaction* mt);
+void decrement_mini_transaction_reference_counter_UNSAFE(mini_transaction_engine* mte, mini_transaction* mt)
+{
+	mt->reference_counter--;
+
+	// if the reference count is non zero, nothign needs to be done
+	if(mt->reference_counter > 0)
+		return;
+
+	if(are_equal_uint256(mt->mini_transaction_id, INVALID_LOG_SEQUENCE_NUMBER)) // it is still a reader
+		remove_from_linkedlist(&(mte->reader_mini_transactions), mt);
+	else // it is a writer
+		remove_from_hashmap(&(mte->writer_mini_transactions), mt);
+
+	// add it to free_list
+	insert_head_in_linkedlist(&(mte->free_mini_transactions_list), mt);
+
+	// wake up anyone waiting for execution slot
+	pthread_cond_signal(&(mte->conditional_to_wait_for_execution_slot));
+}
 
 int wait_for_mini_transaction_completion_UNSAFE(mini_transaction_engine* mte, mini_transaction* mt);
