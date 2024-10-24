7dae990a806b96cb00a8ca465cbfd34b1df25675
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-24 10:53:22 +0530

    added declarations for the new bufferpool only functions

--- inc/mini_transaction_engine_bufferpool_only_functions.h
+++ inc/mini_transaction_engine_bufferpool_only_functions.h
@@ -3,4 +3,13 @@
 
 #include<mini_transaction_engine.h>
 
+void* acquire_page_with_reader_lock_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id);
+void* acquire_page_with_writer_lock_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id);
+
+int downgrade_writer_lock_to_reader_lock_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents);
+int upgrade_reader_lock_to_writer_lock_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents);
+
+int release_reader_lock_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, int free_page);
+int release_writer_lock_on_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, void* page_contents, int free_page);
+
 #endif
