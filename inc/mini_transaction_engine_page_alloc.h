84a960d4c7b9c3ea6a5ca9dd18cc26fb7dffc116
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-25 21:10:39 +0530

    started with implementing free page function

--- inc/mini_transaction_engine_page_alloc.h
+++ inc/mini_transaction_engine_page_alloc.h
@@ -1,4 +1,8 @@
 #ifndef MINI_TRANSACTION_ENGINE_PAGE_ALLOC_H
 #define MINI_TRANSACTION_ENGINE_PAGE_ALLOC_H
 
+#include<mini_transaction_engine.h>
+
+int free_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id);
+
 #endif
--- src/mini_transaction_engine_page_alloc.c
+++ src/mini_transaction_engine_page_alloc.c
@@ -1,1 +1,6 @@
 #include<mini_transaction_engine_page_alloc.h>
+
+int free_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id)
+{
+	// TODO
+}
