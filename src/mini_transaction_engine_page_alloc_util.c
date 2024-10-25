a2e24da589ad65693fb285f9a6606e7e317874b6
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-25 19:24:06 +0530

    started implemenitng free page internal function

--- src/mini_transaction_engine_page_alloc_util.c
+++ src/mini_transaction_engine_page_alloc_util.c
@@ -1,3 +1,8 @@
 #include<mini_transaction_engine_page_alloc_util.h>
 
 #include<mini_transaction_engine_util.h>
+
+int free_write_latched_page_INTERNAL_UNSAFE(mini_transaction_engine* mte, mini_transaction* mt, void* page, uint64_t page_id)
+{
+	// TODO
+}
