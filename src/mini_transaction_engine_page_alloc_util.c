1e7d8bd8c7c56e789c54e767fbeafd96ff3a7e98
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-25 19:59:34 +0530

    constructed the actual log record, now we only need to log it

--- src/mini_transaction_engine_page_alloc_util.c
+++ src/mini_transaction_engine_page_alloc_util.c
@@ -22,19 +22,35 @@ int free_write_latched_page_INTERNAL(mini_transaction_engine* mte, mini_transact
 	perform_full_page_write_for_page_if_necessary_and_manage_state_INTERNAL(mte, mt, page, page_id);
 	perform_full_page_write_for_page_if_necessary_and_manage_state_INTERNAL(mte, mt, free_space_mapper_page, free_space_mapper_page_id);
 
+	// construct page_deallocation log record
+	log_record act_lr = {
+		.type = PAGE_DEALLOCATION,
+		.palr = {
+			.mini_transaction_id = mt->mini_transaction_id,
+			.prev_log_record_LSN = mt->lastLSN,
+			.page_id = page_id,
+		},
+	};
+
+	// serialize log record object
+	uint32_t serialized_act_lr_size = 0;
+	const void* serialized_act_lr = serialize_log_record(&(mte->lrtd), &(mte->stats), &act_lr, &serialized_act_lr_size);
+	if(serialized_act_lr == NULL)
+		exit(-1);
+
 	// reset the free_space_mapper_bit_pos on the free_space_mapper_page
 	{
 		void* free_space_mapper_page_contents = get_page_contents_for_page(free_space_mapper_page, free_space_mapper_page_id, &(mte->stats));
 		reset_bit(free_space_mapper_page_contents, free_space_mapper_bit_pos);
 	}
 
-	// construct page_deallocation log record
-
-
 	// log the page deallocation log record and manage state
 	pthread_mutex_lock(&(mte->global_lock));
 
 	pthread_mutex_unlock(&(mte->global_lock));
+
+	// free the actual change log record
+	free((void*)serialized_act_lr);
 
 	return 1;
 }
