c17c98daef92f73ebbcb4273e2691bc5e52fd41f
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-27 22:04:23 +0530

    implemented logic for page allocation internal function, very similar to free page function

--- src/mini_transaction_engine_page_alloc_util.c
+++ src/mini_transaction_engine_page_alloc_util.c
@@ -82,7 +82,6 @@ int free_write_latched_page_INTERNAL(mini_transaction_engine* mte, mini_transact
 
 		// set the writer LSN to the mini_transaction_id for the page, (marking it as write locked)
 		set_writerLSN_for_page(page, mt->mini_transaction_id, &(mte->stats));
-		// since we got the write latch on the page, either we ourselves have locked the page OR it was not persistently locked by any one
 
 		// mark the page and free_space_mapper_page as dirty in the bufferpool and dirty page table
 		mark_page_as_dirty_in_bufferpool_and_dirty_page_table_UNSAFE(mte, page, page_id);
@@ -110,9 +109,94 @@ int free_write_latched_page_INTERNAL(mini_transaction_engine* mte, mini_transact
 // below function always succeeds or exit(-1)
 // it returns pointer to the page after success
 // after this function returns you will still hold write latch on the page, but the write latch on the free space mapper page will be released
+// this function does not check if the given page is free or not
+// we also do not check if the page is write locked by self or none
+// you must do the above two checks in your calling code
 static void* allocate_page_holding_write_latch_INTERNAL(mini_transaction_engine* mte, mini_transaction* mt, void* free_space_mapper_page, uint64_t free_space_mapper_page_id, void* page, uint64_t page_id)
 {
-	// TODO
+	// ensure correctness of the parameters
+	if(!is_free_space_mapper_page(free_space_mapper_page_id, &(mte->stats)))
+		exit(-1);
+	if(is_free_space_mapper_page(page_id, &(mte->stats)))
+		exit(-1);
+	if(free_space_mapper_page_id != get_is_valid_bit_page_id_for_page(page_id, &(mte->stats)))
+		exit(-1);
+
+	// perform full page writes if necessary
+	perform_full_page_write_for_page_if_necessary_and_manage_state_INTERNAL(mte, mt, page, page_id);
+	perform_full_page_write_for_page_if_necessary_and_manage_state_INTERNAL(mte, mt, free_space_mapper_page, free_space_mapper_page_id);
+
+	// construct page_allocation log record
+	log_record act_lr = {
+		.type = PAGE_ALLOCATION,
+		.palr = {
+			.mini_transaction_id = mt->mini_transaction_id,
+			.prev_log_record_LSN = mt->lastLSN,
+			.page_id = page_id,
+		},
+	};
+
+	// serialize log record object
+	uint32_t serialized_act_lr_size = 0;
+	const void* serialized_act_lr = serialize_log_record(&(mte->lrtd), &(mte->stats), &act_lr, &serialized_act_lr_size);
+	if(serialized_act_lr == NULL)
+		exit(-1);
+
+	// set to 1 the free_space_mapper_bit_pos on the free_space_mapper_page
+	{
+		uint64_t free_space_mapper_bit_pos = get_is_valid_bit_position_for_page(page_id, &(mte->stats));
+		void* free_space_mapper_page_contents = get_page_contents_for_page(free_space_mapper_page, free_space_mapper_page_id, &(mte->stats));
+		set_bit(free_space_mapper_page_contents, free_space_mapper_bit_pos);
+	}
+
+	// log the page allocation log record and manage state
+	pthread_mutex_lock(&(mte->global_lock));
+	{
+		wale* wale_p = &(((wal_accessor*)get_back_of_arraylist(&(mte->wa_list)))->wale_handle);
+
+		int wal_error = 0;
+		uint256 log_record_LSN = append_log_record(wale_p, serialized_act_lr, serialized_act_lr_size, 0, &wal_error);
+		if(are_equal_uint256(log_record_LSN, INVALID_LOG_SEQUENCE_NUMBER)) // exit with failure if you fail to append log record
+			exit(-1);
+
+		if(are_equal_uint256(mt->mini_transaction_id, INVALID_LOG_SEQUENCE_NUMBER))
+		{
+			mt->mini_transaction_id = log_record_LSN;
+
+			// remove mt from mte->reader_mini_transactions
+			remove_from_linkedlist(&(mte->reader_mini_transactions), mt);
+
+			// insert it to mte->writer_mini_transactions
+			insert_in_hashmap(&(mte->writer_mini_transactions), mt);
+		}
+
+		mt->lastLSN = log_record_LSN;
+
+		// since this log record modified both the pages, set page lsn on both of them
+		set_pageLSN_for_page(page, log_record_LSN, &(mte->stats));
+		set_pageLSN_for_page(free_space_mapper_page, log_record_LSN, &(mte->stats));
+
+		// set the writer LSN to the mini_transaction_id for the page, (marking it as write locked)
+		set_writerLSN_for_page(page, mt->mini_transaction_id, &(mte->stats));
+
+		// mark the page and free_space_mapper_page as dirty in the bufferpool and dirty page table
+		mark_page_as_dirty_in_bufferpool_and_dirty_page_table_UNSAFE(mte, page, page_id);
+		mark_page_as_dirty_in_bufferpool_and_dirty_page_table_UNSAFE(mte, free_space_mapper_page, free_space_mapper_page_id);
+
+		// recalculate page checksums, prior to releasing the latches
+		pthread_mutex_unlock(&(mte->global_lock));
+		recalculate_page_checksum(free_space_mapper_page, &(mte->stats));
+		pthread_mutex_lock(&(mte->global_lock));
+
+		// this has to succeed, we already marked it dirty, so was_modified can be set to 0
+		release_writer_lock_on_page(&(mte->bufferpool_handle), free_space_mapper_page, 0, 0); // was_modified = 0, force_flush = 0
+	}
+	pthread_mutex_unlock(&(mte->global_lock));
+
+	// free the actual change log record
+	free((void*)serialized_act_lr);
+
+	return page;
 }
 
 void* allocate_page_without_database_expansion_INTERNAL(mini_transaction_engine* mte, mini_transaction* mt, uint64_t* page_id)
