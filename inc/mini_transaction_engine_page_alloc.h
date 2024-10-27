1189ba3bc0ebf204ef12874b2ec547f5da9b576f
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-27 20:57:42 +0530

    we give out latches and never locks

--- inc/mini_transaction_engine_page_alloc.h
+++ inc/mini_transaction_engine_page_alloc.h
@@ -5,6 +5,6 @@
 
 int free_page_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id);
 
-void* get_new_page_with_write_lock_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t* page_id_returned);
+void* get_new_page_with_write_latch_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t* page_id_returned);
 
 #endif
