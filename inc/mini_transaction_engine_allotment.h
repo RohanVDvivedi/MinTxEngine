f0e66539b493ac40be01014abdd42f5ed36d5c99
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-28 21:48:07 +0530

    new function declarations added

---
+++ inc/mini_transaction_engine_allotment.h
@@ -0,0 +1,16 @@
+#ifndef MINI_TRANSACTION_ENGINE_ALLOTMENT_H
+#define MINI_TRANSACTION_ENGINE_ALLOTMENT_H
+
+// allots a new mini transaction for you to use
+mini_transaction* mte_allot_mini_tx(mini_transaction_engine* mte, uint64_t wait_timeout_in_microseconds);
+
+// completes a mini transaction for you
+// if it is a reader it is directly put into MINI_TX_COMPLETED state
+// if it is a writer that is still in MINI_TX_IN_PROGRESS state, then a COMPLETE_MINI_TX log record is appended, then put into MINI_TX_COMPLETED state
+// if it is a writer in MINI_TX_ABORTED state, then a ABORT_MINI_TX is appended, and then placed in MINI_TX_UNDOING_FOR_ABORT
+//    then if it is in MINI_TX_UNDOING_FOR_ABORT state then COMPESATION_LOG records are generated from its last log record until we reach the beginning
+//    then a COMPLETE_MINI_TX log record is appended, then put into MINI_TX_COMPLETED state
+// finally the mini transaction reference counter is decremented and returned to be used by someone else
+void mte_complete_mini_tx(mini_transaction_engine* mte, mini_transaction* mt);
+
+#endif
