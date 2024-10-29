c1180ace33546d09da03a182e72ce9b269528b6d
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-29 08:56:45 +0530

    need to initialize mini transaction before giving it to the user

--- src/mini_transaction_engine_allotment.c
+++ src/mini_transaction_engine_allotment.c
@@ -44,6 +44,13 @@ mini_transaction* mte_allot_mini_tx(mini_transaction_engine* mte, uint64_t wait_
 		remove_head_from_linkedlist(&(mte->free_mini_transactions_list));
 	}
 
+	mt->mini_transaction_id = INVALID_LOG_SEQUENCE_NUMBER;
+	mt->lastLSN = INVALID_LOG_SEQUENCE_NUMBER;
+	mt->state = MIN_TX_IN_PROGRESS;
+	mt->abort_error = 0;
+	mt->reference_counter = 0;
+	initialize_llnode(&(mt->enode));
+
 	pthread_mutex_unlock(&(mte->global_lock));
 
 	return mt;
