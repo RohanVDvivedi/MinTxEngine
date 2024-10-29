374133759213f50db92e0d50a4e17217d18abb0d
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-29 19:26:34 +0530

    added compilation command for bplus_tree test

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,1 +1,3 @@
-gcc ./test_serde_log_record.c -o testlr.out -lmintxengine -ltuplestore -lwale -lbufferpool -lrwlock -lserint -lcutlery -lpthread
+#gcc ./test_serde_log_record.c -o testlr.out -lmintxengine -ltuplestore -lwale -lbufferpool -lrwlock -lserint -lcutlery -lpthread
+
+gcc ./test_bplus_tree.c -o testbpt.out -lmintxengine -ltupleindexer -ltuplestore -lwale -lbufferpool -lrwlock -lserint -lcutlery -lpthread
