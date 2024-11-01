48d78b1b5b82d84656e99fa05a60c333ab09f333
RohanVDvivedi <rohandvivedi@gmail.com>
2024-11-01 17:56:15 +0530

    added a comment

--- inc/abort_errors_list.h
+++ inc/abort_errors_list.h
@@ -1,6 +1,10 @@
 #ifndef ABORT_ERRORS_LIST_H
 #define ABORT_ERRORS_LIST_H
 
+/*
+	all abort errors must be negative
+*/
+
 #define ILLEGAL_PAGE_ID -1
 #define OUT_OF_BUFFERPOOL_MEMORY -2
 #define PLAUSIBLE_DEADLOCK -3
