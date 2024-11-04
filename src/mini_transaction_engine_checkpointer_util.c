f945386089e62accb98be2bfcc399c95b6443131
RohanVDvivedi <rohandvivedi@gmail.com>
2024-11-04 09:53:48 +0530

    added logic to read all of the checkpoint

--- src/mini_transaction_engine_checkpointer_util.c
+++ src/mini_transaction_engine_checkpointer_util.c
@@ -4,7 +4,103 @@
 
 #include<log_record.h>
 
-uint256 read_checkpoint_from_wal_UNSAFE(mini_transaction_engine* mte, uint256 checkpointLSN, checkpoint* ckpt);
+uint256 read_checkpoint_from_wal_UNSAFE(mini_transaction_engine* mte, uint256 checkpointLSN, checkpoint* ckpt)
+{
+	{
+		if(!initialize_hashmap(&(ckpt->mini_transaction_table), ELEMENTS_AS_LINKEDLIST_INSERT_AT_TAIL, 10, &simple_hasher(hash_mini_transaction), &simple_comparator(compare_mini_transactions), offsetof(mini_transaction, enode)))
+		{
+			printf("ISSUE :: unable to initialize an checkpoint hashmap\n");
+			exit(-1);
+		}
+
+		if(!initialize_hashmap(&(ckpt->dirty_page_table), ELEMENTS_AS_LINKEDLIST_INSERT_AT_TAIL, 10, &simple_hasher(hash_dirty_page_table_entry), &simple_comparator(compare_dirty_page_table_entries), offsetof(dirty_page_table_entry, enode)))
+		{
+			printf("ISSUE :: unable to initialize an checkpoint hashmap\n");
+			exit(-1);
+		}
+	}
+
+	uint256 read_at = checkpointLSN;
+	uint256 begin_LSN = INVALID_LOG_SEQUENCE_NUMBER;
+
+	{
+		log_record lr;
+		if(!get_parsed_log_record_UNSAFE(mte, read_at, &lr))
+		{
+			printf("ISSUE :: could not read checkpoint end log record\n");
+			exit(-1);
+		}
+
+		if(lr.type != CHECKPOINT_END)
+		{
+			printf("ISSUE :: expected a checkpoint end log record type, but found something else, possibly a bug in mini transaction engine\n");
+			exit(-1);
+		}
+
+		read_at = get_prev_log_record_LSN_for_log_record(&lr);
+
+		// initialize begin_LSN
+		begin_LSN = lr.ckptelr.begin_LSN;
+
+		destroy_and_free_parsed_log_record(&lr);
+	}
+
+	// read while read_at != INVALID_LOG_SEQUENCE_NUMBER
+	while(!are_equal_uint256(read_at, INVALID_LOG_SEQUENCE_NUMBER))
+	{
+		log_record lr;
+		if(!get_parsed_log_record_UNSAFE(mte, read_at, &lr))
+		{
+			printf("ISSUE :: could not read checkpoint end log record\n");
+			exit(-1);
+		}
+
+		if(lr.type == CHECKPOINT_MINI_TRANSACTION_TABLE_ENTRY)
+		{
+			mini_transaction* mt = get_new_mini_transaction();
+
+			mt->mini_transaction_id = lr.ckptmttelr.mt.mini_transaction_id;
+			mt->lastLSN = lr.ckptmttelr.mt.lastLSN;
+			mt->state = lr.ckptmttelr.mt.state;
+
+			insert_in_hashmap(&(ckpt->mini_transaction_table), mt);
+
+			if(get_element_count_hashmap(&(ckpt->mini_transaction_table)) / 3 > get_bucket_count_hashmap(&(ckpt->mini_transaction_table)))
+				expand_hashmap(&(ckpt->mini_transaction_table), 1.5);
+		}
+		else if(lr.type == CHECKPOINT_DIRTY_PAGE_TABLE_ENTRY)
+		{
+			dirty_page_table_entry* dpte = get_new_dirty_page_table_entry();
+
+			dpte->page_id = lr.ckptdptelr.dpte.page_id;
+			dpte->recLSN = lr.ckptdptelr.dpte.recLSN;
+
+			insert_in_hashmap(&(ckpt->dirty_page_table), dpte);
+
+			if(get_element_count_hashmap(&(ckpt->dirty_page_table)) / 3 > get_bucket_count_hashmap(&(ckpt->dirty_page_table)))
+				expand_hashmap(&(ckpt->dirty_page_table), 1.5);
+		}
+		else
+		{
+			printf("ISSUE :: expected a checkpoint entry log record type, but found something else, possibly a bug in mini transaction engine\n");
+			exit(-1);
+		}
+
+		// if begin_LSN == read_at -> i.e. it is the oldest log record of the checkpoint
+		// and the previous log record of this checkpoint is not INVALID, then the checkpoint is probably corrupted
+		if(are_equal_uint256(begin_LSN, read_at) && !are_equal_uint256(get_prev_log_record_LSN_for_log_record(&lr), INVALID_LOG_SEQUENCE_NUMBER))
+		{
+			printf("ISSUE :: begin_LSN not the checkpoint's first log record, probably a bug in mini transaction engine\n");
+			exit(-1);
+		}
+
+		read_at = get_prev_log_record_LSN_for_log_record(&lr);
+
+		destroy_and_free_parsed_log_record(&lr);
+	}
+
+	return begin_LSN;
+}
 
 uint256 append_checkpoint_to_wal_UNSAFE(mini_transaction_engine* mte, const checkpoint* ckpt, uint256* begin_LSN)
 {
