33008d0f50333414493ca784fd6fc2eb44a9bb72
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-09 18:51:16 +0530

    --

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
-#gcc -Wall ./test_serde_log_record.c -o testlr.out -lmintxengine -ltuplestore -lwale -lz -lbufferpool -lblockio -lboompar -lrwlock -lserint -lcutlery -lpthread
+gcc -Wall ./test_serde_log_record.c -o testlr.out -lmintxengine -ltuplestore -lwale -lz -lbufferpool -lblockio -lboompar -lrwlock -lserint -lcutlery -lpthread
 
 gcc -Wall ./test_mindb.c -o test_mindb.out -I. -lmintxengine -ltupleindexer -ltuplestore -lwale -lz -lbufferpool -lblockio -lboompar -lrwlock -lserint -lcutlery -lpthread
