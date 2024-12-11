36717d9c6ca1ac17fcb469b5bcfd202657e3bed1
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-11 10:27:04 +0530

    -- minor modification

--- test/test_tuple_infos.h
+++ test/test_tuple_infos.h
@@ -116,32 +116,17 @@ void construct_record(void* buffer, uint64_t num, int order, char* value)
 {
 	init_tuple(&record_def, buffer);
 
-	int index = 0;
-
-	if(index < RECORD_ELEMENT_COUNT)
-	{
-		set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, &(user_value){.uint_value = num}, UINT32_MAX);
-		index++;
-	}
+	set_element_in_tuple(&record_def, STATIC_POSITION(0), buffer, &(user_value){.uint_value = num}, UINT32_MAX);
 
 	uint16_t o = find_order(num, order);
-	if(index  < RECORD_ELEMENT_COUNT)
-	{
-		set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, &(user_value){.int_value = order}, UINT32_MAX);
-		index++;
-	}
+	set_element_in_tuple(&record_def, STATIC_POSITION(1), buffer, &(user_value){.int_value = order}, UINT32_MAX);
 
-	if(index < RECORD_ELEMENT_COUNT)
-	{
-		char temp[100];
-		num_in_words(temp, o);
-		set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, &(user_value){.string_value = temp, .string_size = strlen(temp)}, UINT32_MAX);\
-		index++;
-	}
+	char temp[100];
+	num_in_words(temp, o);
+	set_element_in_tuple(&record_def, STATIC_POSITION(2), buffer, &(user_value){.string_value = temp, .string_size = strlen(temp)}, UINT32_MAX);
 
-	if(index < RECORD_ELEMENT_COUNT)
 	{
-		set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, EMPTY_USER_VALUE, UINT32_MAX);
+		set_element_in_tuple(&record_def, STATIC_POSITION(3), buffer, EMPTY_USER_VALUE, UINT32_MAX);
 		uint32_t size = 0;
 		uint32_t digits[64];
 		while(num > 0)
@@ -149,20 +134,15 @@ void construct_record(void* buffer, uint64_t num, int order, char* value)
 			digits[size++] = num % 10;
 			num = num / 10;
 		}
-		expand_element_count_for_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, 0, size, UINT32_MAX);
+		expand_element_count_for_element_in_tuple(&record_def, STATIC_POSITION(3), buffer, 0, size, UINT32_MAX);
 		for(uint32_t i = 0; i < size; i++)
-			set_element_in_tuple(&record_def, STATIC_POSITION(index,i), buffer, &(user_value){.uint_value = digits[i]}, UINT32_MAX);
-		index++;
+			set_element_in_tuple(&record_def, STATIC_POSITION(3,i), buffer, &(user_value){.uint_value = digits[i]}, UINT32_MAX);
 	}
 
-	if(index < RECORD_ELEMENT_COUNT)
-	{
-		if(value == NULL)
-			set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, NULL_USER_VALUE, UINT32_MAX);
-		else
-			set_element_in_tuple(&record_def, STATIC_POSITION(index), buffer, &(user_value){.string_value = value, .string_size = strlen(value)}, UINT32_MAX);
-		index++;
-	}
+	if(value == NULL)
+		set_element_in_tuple(&record_def, STATIC_POSITION(4), buffer, NULL_USER_VALUE, UINT32_MAX);
+	else
+		set_element_in_tuple(&record_def, STATIC_POSITION(4), buffer, &(user_value){.string_value = value, .string_size = strlen(value)}, UINT32_MAX);
 }
 
 int validate_record(const void* buffer)
