14398d2914a2f50137291b9d2d6b4bf0fae997b9
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-13 20:03:22 +0530

    building a null version to return a page after latching it with read latch, while no-one hold write lock on it

--- src/mini_transaction_engine_bufferpool_only_functions.c
+++ src/mini_transaction_engine_bufferpool_only_functions.c
@@ -3,7 +3,7 @@
 #include<mini_transaction_engine_util.h>
 #include<system_page_header_util.h>
 
-void* acquire_page_with_reader_latch_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id)
+static void* acquire_page_with_reader_latch_for_mini_tx_MT_NULL(mini_transaction_engine* mte, uint64_t page_id)
 {
 	pthread_mutex_lock(&(mte->global_lock));
 		shared_lock(&(mte->manager_lock), READ_PREFERRING, BLOCKING);
@@ -98,6 +98,111 @@ void* acquire_page_with_reader_latch_for_mini_tx(mini_transaction_engine* mte, m
 	if(latched_page == NULL)
 		return NULL;
 	return latched_page + get_system_header_size_for_data_pages(&(mte->stats));
+}
+
+static void* acquire_page_with_reader_latch_for_mini_tx_MT_NON_NULL(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id)
+{
+	pthread_mutex_lock(&(mte->global_lock));
+		shared_lock(&(mte->manager_lock), READ_PREFERRING, BLOCKING);
+
+		// mini transaction is not in progress, then quit
+		if(mt->state != MIN_TX_IN_PROGRESS)
+		{
+			shared_unlock(&(mte->manager_lock));
+			pthread_mutex_unlock(&(mte->global_lock));
+			return NULL;
+		}
+
+		// if you are attempting to lock a free space mapper page, then abort and quit
+		if(is_free_space_mapper_page(page_id, &(mte->stats)))
+		{
+			mt->state = MIN_TX_ABORTED;
+			mt->abort_error = ILLEGAL_PAGE_ID;
+			shared_unlock(&(mte->manager_lock));
+			pthread_mutex_unlock(&(mte->global_lock));
+			return NULL;
+		}
+
+		// you must not cross max page count, else abort and quit
+		if(page_id >= mte->user_stats.max_page_count)
+		{
+			mt->state = MIN_TX_ABORTED;
+			mt->abort_error = ILLEGAL_PAGE_ID;
+			shared_unlock(&(mte->manager_lock));
+			pthread_mutex_unlock(&(mte->global_lock));
+			return NULL;
+		}
+
+		// check to ensure that you are not attempting to latch a page that is out of bounds for the current page count
+		if(page_id >= mte->database_page_count) // this check must be done with manager_lock held
+		{
+			mt->state = MIN_TX_ABORTED;
+			mt->abort_error = ILLEGAL_PAGE_ID;
+			shared_unlock(&(mte->manager_lock));
+			pthread_mutex_unlock(&(mte->global_lock));
+			return NULL;
+		}
+
+		void* latched_page = NULL;
+		uint64_t write_lock_wait_timeout_in_microseconds_LEFT = mte->write_lock_wait_timeout_in_microseconds;
+		while(1)
+		{
+			// attempt to acquire latch on this page with page_id
+			latched_page = acquire_page_with_reader_latch_N_flush_wal_if_necessary_UNSAFE(mte, page_id, 1); // evict_dirty_if_necessary
+			if(latched_page == NULL)
+			{
+				mt->state = MIN_TX_ABORTED;
+				mt->abort_error = OUT_OF_BUFFERPOOL_MEMORY;
+				break;
+			}
+
+			// check who has persistent write lock on it
+			mini_transaction* mt_locked_by = get_mini_transaction_that_last_persistent_write_locked_this_page_UNSAFE(mte, latched_page);
+			if(mt_locked_by == NULL || mt_locked_by == mt) // if locked by self or NULL, we are done
+				break;
+
+			// release latch on the latched page, this must succeed
+			release_reader_lock_on_page(&(mte->bufferpool_handle), latched_page);
+			latched_page = NULL;
+
+			if(write_lock_wait_timeout_in_microseconds_LEFT == 0) // no wait attempts left
+			{
+				mt->state = MIN_TX_ABORTED;
+				mt->abort_error = PLAUSIBLE_DEADLOCK;
+				break;
+			}
+
+			// wait for completion of a mt_locked_by mini transaction
+			if(!wait_for_mini_transaction_completion_UNSAFE(mte, mt_locked_by, &write_lock_wait_timeout_in_microseconds_LEFT))
+			{
+				// comes here when we time out, this could be because of a PLAUSIBLE_DEADLOCK
+				mt->state = MIN_TX_ABORTED;
+				mt->abort_error = PLAUSIBLE_DEADLOCK;
+				break;
+			}
+
+			// we waited until completion of a mini transaction, we must try again and test if the latch could be acquired without any contention, so continue
+			continue;
+		}
+
+		// a new page latch is granted, so increment the latch counter for this mini transaction
+		if(latched_page != NULL)
+			mt->page_latches_held_counter++;
+
+		shared_unlock(&(mte->manager_lock));
+	pthread_mutex_unlock(&(mte->global_lock));
+
+	if(latched_page == NULL)
+		return NULL;
+	return latched_page + get_system_header_size_for_data_pages(&(mte->stats));
+}
+
+void* acquire_page_with_reader_latch_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id)
+{
+	if(mt == NULL)
+		return acquire_page_with_reader_latch_for_mini_tx_MT_NULL(mte, page_id);
+	else
+		return acquire_page_with_reader_latch_for_mini_tx_MT_NON_NULL(mte, mt, page_id);
 }
 
 void* acquire_page_with_writer_latch_for_mini_tx(mini_transaction_engine* mte, mini_transaction* mt, uint64_t page_id)
