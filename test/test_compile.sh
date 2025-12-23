7fdf8eb8173ec0637caeed1b985a5a885f3270f5
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-23 21:16:25 +0530

    basic page allocation hints test cases

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,5 @@
 gcc -Wall ./test_serde_log_record.c -o testlr.out -lmintxengine -ltuplestore -lwale -lz -lbufferpool -lblockio -lboompar -llockking -lcutlery -lpthread
 
 gcc -Wall ./test_mindb.c -o test_mindb.out -I. -lmintxengine -ltupleindexer -ltuplestore -lwale -lz -lbufferpool -lblockio -lboompar -llockking -lcutlery -lpthread
+
+gcc -Wall ./test_pah.c -o test_pah.out -I. -lmintxengine -lbufferpool -lblockio -lboompar -llockking -lcutlery -lpthread
