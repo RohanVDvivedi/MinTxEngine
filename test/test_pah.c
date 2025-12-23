72e0002048c4bcdb401d830dd38e13549bcac72b
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-23 21:20:42 +0530

    added hints file to gitignore

--- test/test_pah.c
+++ test/test_pah.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	page_allocation_hints* pah_p = get_new_page_allocation_hints("./db.hints");
+	page_allocation_hints* pah_p = get_new_page_allocation_hints("./test.db_hints");
 
 	update_hints_for_extents(pah_p, (uint64_t[]){0,2,3,65000,95000,95001,400000,400003}, 8, (uint64_t[]){1,9,12,65002,94999,95002,400040,400055}, 8);
 
