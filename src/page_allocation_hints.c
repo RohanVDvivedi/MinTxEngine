9b336f49596951fa9f1a462b09403f84eef8fae3
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-23 22:49:36 +0530

    page allocaton hints can be hinted at what number of pages to buffer

--- src/page_allocation_hints.c
+++ src/page_allocation_hints.c
@@ -591,7 +591,7 @@ page_allocation_hints* get_new_page_allocation_hints(uint64_t max_pages_to_buffe
 	}
 
 	// create a bufferpool
-	if(!initialize_bufferpool(&(pah_p->bf), max_pages_to_buffer, NULL, get_page_io_ops((&(pah_p->extent_allocation_hints_file))), can_hint_page_be_flushed_to_disk, hint_page_was_flushed_to_disk, NULL, 60 * 1000000, HINTS_FRAMES_TO_CACHE)) // flush all frames every minute, unless there is overload
+	if(!initialize_bufferpool(&(pah_p->bf), max_pages_to_buffer, NULL, get_page_io_ops((&(pah_p->extent_allocation_hints_file))), can_hint_page_be_flushed_to_disk, hint_page_was_flushed_to_disk, NULL, 60 * 1000000, max_pages_to_buffer)) // flush all frames every minute, unless there is overload
 	{
 		close_block_file(&(pah_p->extent_allocation_hints_file));
 		free(pah_p);
