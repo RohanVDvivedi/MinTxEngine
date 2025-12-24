b30fbb0d743f92128ed9ac0d7b4f609b4210af2f
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-24 21:41:51 +0530

    added new api suggested params

--- test/test_pah.c
+++ test/test_pah.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	page_allocation_hints* pah_p = get_new_page_allocation_hints(100, "./test.db_hints");
+	page_allocation_hints* pah_p = get_new_page_allocation_hints(100, "./test.db_hints", 1000, 1000);
 
 	update_hints_for_extents(pah_p, (uint64_t[]){0,2,3,65000,95000,95001,400000,400003, UINT64_C(11564439971254894642)}, 9, (uint64_t[]){1,9,12,65002,94999,95002,400040,400055, UINT64_C(564439971254894642)}, 9);
 
