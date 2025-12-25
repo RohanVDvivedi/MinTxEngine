0e6cbeffaadda6b40b4c26d2ab6cb341d45bc09c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-25 17:55:26 +0530

    added freeing based test cases

--- test/test_pah.c
+++ test/test_pah.c
@@ -9,9 +9,62 @@ int main()
 
 	update_hints_for_extents(pah_p, (uint64_t[]){0,2,3,65000,95000,95001,400000,400003, UINT64_C(11564439971254894642)}, 9, (uint64_t[]){1,9,12,65002,94999,95002,400040,400055, UINT64_C(564439971254894642)}, 9);
 
+	printf("\n\n\n\n\n");
+
 	update_hints_for_extents(pah_p, NULL, 0, (uint64_t[]){0,1,9,12,65010,94999,95002,400040,400055, UINT64_C(564439971254894642), UINT64_C(9223372036854775805), UINT64_C(9223372036854775815), UINT64_C(11564439971254894642), UINT64_C(11564439971254894643), UINT64_MAX-4, UINT64_MAX-5}, 16);
 
-	printf("\n\n\n");
+	printf("\n\n\n\n\n");
+
+	uint64_t result[50];
+	uint64_t result_size = 0;
+
+	result_size = 20;
+	find_free_extents(pah_p, 0, result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 20;
+	find_free_extents(pah_p, 65000, result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 5;
+	find_free_extents(pah_p, 95000, result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 50;
+	find_free_extents(pah_p, 400000, result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 20;
+	find_free_extents(pah_p, UINT64_C(11564439971254894640), result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 20;
+	find_free_extents(pah_p, UINT64_C(9223372036854775800), result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	result_size = 15;
+	find_free_extents(pah_p, UINT64_MAX-8, result, &result_size);
+	for(uint64_t i = 0; i < result_size; i++)
+		printf("%"PRIu64"\n", result[i]);
+	printf("\n");
+
+	printf("\n\n\n\n\n");
+
+	update_hints_for_extents(pah_p, (uint64_t[]){0,1,9,12,65010,94999,95002,400040,400055, UINT64_C(564439971254894642), UINT64_C(9223372036854775805), UINT64_C(9223372036854775815), UINT64_C(11564439971254894642), UINT64_C(11564439971254894643), UINT64_MAX-4, UINT64_MAX-5}, 16,  NULL, 0);
+
+	printf("\n\n\n\n\n");
 
 	uint64_t result[50];
 	uint64_t result_size = 0;
