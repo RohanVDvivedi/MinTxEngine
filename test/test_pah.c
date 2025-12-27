d557ced34fae59a493ccd80929582264d17284a5
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-27 17:32:57 +0530

    new api testing

--- test/test_pah.c
+++ test/test_pah.c
@@ -1,112 +1,36 @@
 #include<mintxengine/page_allocation_hints.h>
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<inttypes.h>
+
+#define MAX_EXTENT_ID 100
+
+#define OUTER_ITERATIONS 10
+#define INNER_ITERATIONS 10
+
+#define RESULTS_SIZE 20
 
 int main()
 {
-	page_allocation_hints* pah_p = get_new_page_allocation_hints(100, "./test.db_hints", 1000, 1000);
+	page_allocation_hints* pah_p = get_new_page_allocation_hints(100, "./test.db_hints", 1024, 1024);
 
-	update_hints_for_extents(pah_p, (uint64_t[]){0,2,3,65000,95000,95001,400000,400003, UINT64_C(11564439971254894642)}, 9, (uint64_t[]){1,9,12,65002,94999,95002,400040,400055, UINT64_C(564439971254894642)}, 9);
+	for(int i = 0; i < OUTER_ITERATIONS; i++)
+	{
+		for(int j = 0; j < INNER_ITERATIONS; j++)
+		{
+			uint64_t extent_id = ((unsigned int)rand()) % MAX_EXTENT_ID;
+			uint64_t free_pages_count_in_extent = rand() % 2;
+			printf("%"PRIu64" -> %d\n", extent_id, (free_pages_count_in_extent == 0));
+			update_hints_in_page_allocation_hints(pah_p, extent_id, free_pages_count_in_extent);
+		}
 
-	printf("\n\n\n\n\n");
-
-	update_hints_for_extents(pah_p, NULL, 0, (uint64_t[]){0,1,9,12,65010,94999,95002,400040,400055, UINT64_C(564439971254894642), UINT64_C(9223372036854775805), UINT64_C(9223372036854775815), UINT64_C(11564439971254894642), UINT64_C(11564439971254894643), UINT64_MAX-4, UINT64_MAX-5}, 16);
-
-	printf("\n\n\n\n\n");
-
-	uint64_t result[50];
-	uint64_t result_size = 0;
-
-	result_size = 20;
-	find_free_extents(pah_p, 0, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, 65000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 5;
-	find_free_extents(pah_p, 95000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 50;
-	find_free_extents(pah_p, 400000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, UINT64_C(11564439971254894640), result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, UINT64_C(9223372036854775800), result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 15;
-	find_free_extents(pah_p, UINT64_MAX-8, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	printf("\n\n\n\n\n");
-
-	update_hints_for_extents(pah_p, (uint64_t[]){0,1,9,12,65010,94999,95002,400040,400055, UINT64_C(564439971254894642), UINT64_C(9223372036854775805), UINT64_C(9223372036854775815), UINT64_C(11564439971254894642), UINT64_C(11564439971254894643), UINT64_MAX-4, UINT64_MAX-5}, 16,  NULL, 0);
-
-	printf("\n\n\n\n\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, 0, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, 65000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 5;
-	find_free_extents(pah_p, 95000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 50;
-	find_free_extents(pah_p, 400000, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, UINT64_C(11564439971254894640), result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 20;
-	find_free_extents(pah_p, UINT64_C(9223372036854775800), result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
-
-	result_size = 15;
-	find_free_extents(pah_p, UINT64_MAX-8, result, &result_size);
-	for(uint64_t i = 0; i < result_size; i++)
-		printf("%"PRIu64"\n", result[i]);
-	printf("\n");
+		uint64_t free_extent_ids[RESULTS_SIZE];
+		uint64_t free_extent_ids_count = suggest_extents_from_page_allocation_hints(pah_p, free_extent_ids, RESULTS_SIZE);
+		for(uint64_t i = 0; i < free_extent_ids_count; i++)
+			printf("%"PRIu64" ", free_extent_ids[i]);
+		printf("\n\n");
+	}
 
 	flush_and_delete_page_allocation_hints(pah_p);
 
